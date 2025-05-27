5a38396c1292b0576ec590a16839de7954a5c0d0
Pronab <sarkerpronab2@gmail.com>
2025-05-27 21:54:08 +1000

    The overview and skeleton structure of the programme

---
+++ Question1.py
@@ -0,0 +1,22 @@
+'''
+Overview:
+1. Import some neccessary package, add some other neccessary package along the development of the program
+2. Create classes for image processing, image displaying, image croping etc requirements
+3. In each class, create specific functions that fulfil the requirements of the program
+4. In each function and each module, check the user input and other possible error
+5. Try to optimize the whole code
+'''
+
+import tkinter as tk
+from tkinter import filedialog, ttk, messagebox
+import cv2
+import numpy as np
+
+class ImageProcessor:
+
+class ImageDisplay:
+
+class ImageEditorApp:
+
+
+if __name__ == "__main__":
